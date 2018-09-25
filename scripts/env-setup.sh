6eca8396f437b91fe293eea8058675a686edaaee
nsa <nuno.s@l4b-software.com>
2018-09-25 14:27:18 +0200

    scripts: env_scrip refactoring
    Fixed some bugs present on the script.
    Replaced all spaces by tabs.
    Remove the option of passing the out dir as parameter. All out's
    directoties are handles by the script when a target is added.
    
    Signed-off-by: nsa <nuno.s@l4b-software.com>

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -5,7 +5,10 @@ WHITE='\033[1;37m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-TARGETS=( HOST RPI )
+# This is still an early stage meaning that there isn't
+# a clear idea on how the targets will be managed. For now add them in this array.
+# This target concept will probably change if the future by a project or device concept...
+TARGETS=(HOST RPI)
 
 function __include_all()
 {
@@ -13,25 +16,27 @@ function __include_all()
 	local dir="";
 	local subdir="";
 
-	pushd ${SRCS_DIR} 1>/dev/null;
-	
+	pushd "${SRCS_DIR}" 1>/dev/null;
+
 	for dir in $(find . -maxdepth 1 -mindepth 1 -type d -printf '%f ')
 	do
-		pushd ${dir} 1>/dev/null;
+		pushd "${dir}" 1>/dev/null;
 		for subdir in $(find . -maxdepth 1 -mindepth 1 -type d -printf '%f ')
 		do
-			touch ${subdir}/.detect;
+			touch "${subdir}/.detect";
 		done
-		popd 1>/dev/null;	
+		popd 1>/dev/null;
 	done
 
 	popd 1>/dev/null;
+
+	return 0;
 }
 
 function __is_supported_target()
 {
 	local target=""
-	
+
 	for target in ${TARGETS[*]}
 	do
 		[ "${1}" == "${target}" ] && return 0;
@@ -40,15 +45,18 @@ function __is_supported_target()
 	return 1;
 }
 
-function __setup_common()
+function __create_out_dirs()
 {
-	#may be overriden if --outdir is given
-	export OUT_DIR=${BUILD_ROOT}/out/;
+	export OUT_DIR=${BUILD_ROOT}/out;
+	# let's create the output directories
+	mkdir -p "${OUT_DIR}/${1}/bin"
+	mkdir -p "${OUT_DIR}/${1}/lib"
+	mkdir -p "${OUT_DIR}/${1}/obj"
 }
 
 function __setup_compiler()
 {
-	hash ${1}gcc 2>/dev/null || { echo "Make sure you have ${1} on your system!!!"; return 1; }
+	hash "${1}"gcc 2>/dev/null || { echo "Make sure you have ${1}gcc on your system!!!"; return 1; }
 
 	local CROSS_COMPILER="${1}";
 
@@ -64,29 +72,34 @@ function __setup_compiler()
 	export OBJCOPY=${CROSS_COMPILER}objcopy
 	export OBJDUMP=${CROSS_COMPILER}objdump
 	export RANLIB=${CROSS_COMPILER}ranlib
-	
+
 	return 0;
 }
 
 function __setup_host()
-{	
-	#setup compiler
+{
+	# setup compiler
 	__setup_compiler;
 	[ $? == 1 ] && return 1;
-	__setup_common;
+	# if the target is the host, then we just need
+	# to call this once
+	__create_out_dirs host;
 	export TARGET_BUILD=HOST
+
+	return 0;
 }
 
 function __check_ownerchip()
 {
 	[ $# -ne 1 ] && { echo "Invalid number of parameters in __check_ownerchip" ; return 1; }
-	local owner=$(ls -ld "$1" | awk '{print $3}');
-	
+	local owner='';
+
+	owner=$(ls -ld "$1" | awk '{print $3}');
 	if [ "${owner}" == "root" ]
 	then
 		local opt;
-		echo -en "${BLUE}Owner of "$1" is the root user. Are you sure you want to use this path [y]? ${NC}"
-		read opt;
+		echo -en "${BLUE}Owner of \"$1\" is the root user. Are you sure you want to use this path [y]? ${NC}"
+		read -r opt;
 		if [ "${opt}" == "y" ]
 		then
 			return 0;
@@ -94,62 +107,58 @@ function __check_ownerchip()
 			return 1;
 		fi
 	fi
-	
+
 	return 0;
 }
 
 function __process_args()
 {
-	[ $# -ne 2 ] && { echo "Invalid number of parameters in __process_args" ; return; }
-	
+	[ $# -ne 2 ] && { echo "Invalid number of parameters in __process_args" ; return 1; }
+
 	case "${1}" in
 		"--rootdir")
-			[ ! -d "${2}" ] && {echo -e "${RED}[ERROR]: Invalid root dir="$2"", return };
-			__check_ownerchip "${2}";		
-			[ $? == 1 ] && return;
-			export ROOT_DIR=$(realpath ${2})/;
+			[ ! -d "${2}" ] && { echo -e "${RED}[ERROR]: Make sure to create rootdir=\"$2\"${NC}"; return 1; }
+			__check_ownerchip "${2}";
+			[ $? == 1 ] && return 1;
+			export ROOT_DIR="$(realpath "${2}")/";
 			[[ -n "${INSTALL_LIB_DIR}" && -n "${INSTALL_BIN_DIR}" ]] && { \
-			       	echo -e "${WHITE}[WARNING]: Roodir defined but useless since both bin and lib directories were explicity configured!${NC}"; }
-		;;
-		"--outdir")
-			[ ! -d "${2}" ] && {echo -e "${RED}[ERROR]: Invalid out dir="$2"", return };
-                        __check_ownerchip "${2}";
-                        [ $? == 1 ] && return;
-			export OUT_DIR=$(realpath ${2})/;		
+				echo -e "${WHITE}[WARNING]: Rootdir defined but useless since both bin and lib directories were explicity configured!${NC}"; }
 		;;
 		"--libdir")
-			[ ! -d "${2}" ] && {echo -e "${RED}[ERROR]: Invalid lib dir="$2"", return };
+			[ ! -d "${2}" ] && { echo -e "${RED}[ERROR]: Make sure to create libdir="\"$2\""${MC}"; return 1; }
                         __check_ownerchip "${2}";
-                        [ $? == 1 ] && return;
-                        export INSTALL_LIB_DIR=$(realpath ${2})/;
+                        [ $? == 1 ] && return 1;
+                        export INSTALL_LIB_DIR="$(realpath ${2})/";
 			[[ -n "${ROOT_DIR}" && -n "${INSTALL_BIN_DIR}" ]] && { \
                                 echo -e "${WHITE}[WARNING]: Roodir defined but useless since both bin and lib directories were explicity configured!${NC}"; }
 		;;
 		"--bindir")
-			[ ! -d "${2}" ] && {echo -e "${RED}[ERROR]: Invalid bin dir="$2"", return };
+			[ ! -d "${2}" ] && { echo -e "${RED}[ERROR]: Make sure to create bindir=\"$2\"${NC}"; return 1; }
                         __check_ownerchip "${2}";
-                        [ $? == 1 ] && return;
-                        export INSTALL_BIN_DIR=$(realpath ${2})/;
+                        [ $? == 1 ] && return 1;
+                        export INSTALL_BIN_DIR="$(realpath ${2})/";
 			[[ -n "${INSTALL_LIB_DIR}" && -n "${ROOT_DIR}" ]] && { \
-                               	echo -e "${WHITE}[WARNING]: Roodir defined but useless since both bin and lib directories were explicity configured!${NC}"; }
+				echo -e "${WHITE}[WARNING]: Roodir defined but useless since both bin and lib directories were explicity configured!${NC}"; }
 
 		;;
 		*)
-			echo -e "${RED}[ERROR]: Invalid key=${1}${NC}";	
+			echo -e "${RED}[ERROR]: Invalid key=\"${1}\"${NC}";
 		;;
-	esac	
-}	
+	esac
+
+	return 0;
+}
 
 function __add_target_complete()
 {
 	local cur="${COMP_WORDS[COMP_CWORD]}";
 	local last="${COMP_WORDS[COMP_CWORD-1]}";
-	local opts=('--rootdir=' '--outdir=' '--libdir=' '--bindir=' HOST RPI);	
-	
-	#at this point we want to autocomplete with directories
+	local opts=('--rootdir=' '--outdir=' '--libdir=' '--bindir=' HOST RPI);
+
+	# at this point we want to autocomplete with directories
 	if [[ "${cur}" == "=" || "${last}" == "=" ]]
 	then
-		#echo "wait now for dir, cur=${cur}";
+		# echo "wait now for dir, cur=${cur}";
 		compopt -o filenames;
 		if [ "${cur}" == "=" ]; then
 			COMPREPLY=( $(compgen -d) );
@@ -159,14 +168,14 @@ function __add_target_complete()
 
 		return 0;
 	fi
-	
+
 	[ -n "$(echo "${cur}" | egrep '^--')" ] && compopt -o nospace;
 	COMPREPLY=( $(compgen -W "${opts[*]}" -- ${cur}) );
-	return 0;	
+	return 0;
 }
 
-##based on aosp
-##xargs 0 tells that the inputs are terminated by null char and not whitespace. Important because of -print0
+# based on aosp
+# xargs 0 tells that the inputs are terminated by null char and not whitespace. Important because of -print0
 function cgrep()
 {
 	find . -name .git -prune -o -type f \( -name '*.c' -o -name '*.h' -o -name '*.cpp' -o -name '*.cc' -o -name '*.hpp' \) -print0 | xargs -0 grep --colour -n "$@"
@@ -179,9 +188,9 @@ function mgrep()
 function gettop()
 {
 	local TOPFILE=scripts/env-setup.sh
-	if [ -n "${BUILD_ROOT}" -a "${BUILD_ROOT}/${TOPFILE}" ]
+	if [ -n "${BUILD_ROOT}" ] && [ -d "${BUILD_ROOT}/${TOPFILE}" ]
 	then
-		##build_root should be the physical path and not a symlink
+		# build_root should be the physical path and not a symlink
 		echo "${BUILD_ROOT}";
 	else
 		if [ -f "${TOPFILE}" ]
@@ -190,30 +199,30 @@ function gettop()
 		else
 			local HERE=$PWD;
 			T="";
-			while [ ! -f "${TOPFILE}" -a $PWD != "/" ]
+			while [ ! -f "${TOPFILE}" ] && [ $PWD != "/" ]
 			do
 				cd ..;
 				#-P to avoid symlinks
 				T=`PWD= /bin/pwd -P`;
 			done
-			cd ${HERE};
+			cd "${HERE}";
 			if [ -f "${T}/${TOPFILE}" ]
 			then
-				echo $T;
+				echo "$T";
 			fi
 		fi
-	fi	
+	fi
 }
 
 function croot()
 {
 	local T=$(gettop)
-	if [ -d ${T} ]
+	if [ -d "${T}" ]
 	then
-		cd ${T};
+		cd "${T}" || return;
 	else
 		echo "${WHITE}Couldn't locate the top of the tree. $T${NC}";
-	fi	
+	fi
 }
 
 function clean_env()
@@ -252,57 +261,61 @@ function add_compilable()
 	fi
 
 	tmp=$(realpath "${1}" --relative-to="${BUILD_ROOT}" | awk -F"/" '{print $1}');
-	
+
 	[ "${tmp}" == ".." ] && { echo -e "${WHITE}[WARNING]: "$1" is not a directory under the Sdk root directory${NC}"; return 1; }
 
 	dir=$(echo "${1}" | sed -e 's/\/$//');
-	touch ${dir}/.detect;	
+	touch ${dir}/.detect;
 }
 
 function add_target_build()
 {
 	[ $# -ge 1 ] || { echo "[ERROR]: Invalid number of parameters"; return 1; }
-	local arg="";
-	local key="";
-	local value="";
-	#For now is assumed that the target is always the last argument
-	local target="";
-	local error="";
+	local arg='';
+	local key='';
+	local value='';
+	local target='';
+	local error='';
 
-	###look for passed arguments
-        for arg in $@
+	# clean previous env
+	clean_env;
+	# look for passed arguments
+        for arg in "$@"
         do
-		__is_supported_target ${arg};
+		__is_supported_target "${arg}";
                 [ $? == "0" ] && { \
 			if [ -n "${target}" ]; then
 				echo -e "${RED}[ERROR]: More than one valid target passed on the argument list! Args="$@"${NC}";
 				error=y;
 				break;
 			fi
-			target=${arg}; continue; }
+			target="${arg}"; continue; }
                 key=$(echo "${arg}" | cut -d"=" -f1);
                 value=$(echo "${arg}" | cut -d"=" -f2);
-                __process_args "${key}" "${value}";
+                if ! __process_args "${key}" "${value}"; then
+			error=y;
+			break;
+		fi
         done
-	
+
 	[ "${error}" == "y" ] && { clean_env; return 1; }
-	
-	case "${target}" in 
-	
+
+	case "${target}" in
+
 		"HOST")
 			__setup_host;
-			[ $? == 1 ] && return 1;
+			[ $? == 1 ] && { clean_env; return 1; }
 		;;
 		"RPI")
 			echo "RPI is not yet supportted";
-		;;	
+		;;
 		*)
 			echo -e "${RED}[ERROR]: Unknown TARGET to build for:${1}!${NC}";
 			clean_env;
 			return 1;
 		;;
 	esac
-		
+
 	return 0
 }
 complete -F __add_target_complete add_target_build;
@@ -321,7 +334,7 @@ function print_env()
 	echo "OUT_DIR=${OUT_DIR}";
 	echo "ROOT_DIR=${ROOT_DIR}";
 	if [ -n "${INSTALL_LIB_DIR}" ]; then
-		echo "INSTALL_LIB_DIR=${INSTALL_LIB_DIR}"	
+		echo "INSTALL_LIB_DIR=${INSTALL_LIB_DIR}"
 	elif [ -n "${ROOT_DIR}" ]; then
 		echo "INSTALL_LIB_DIR=${ROOT_DIR}usr/lib";
 	fi
@@ -330,10 +343,10 @@ function print_env()
 		echo "INSTALL_BIN_DIR=${INSTALL_BIN_DIR}";
         elif [ -n "${ROOT_DIR}" ]; then
 		echo "INSTALL_BIN_DIR=${ROOT_DIR}usr/bin";
-        fi	
+	fi
 }
 
-if [ "$UID" == "0" ]
+if [ "$UID" -eq "0" ]
 then
 	echo "${WHITE}WARNING: You are running as root!!${NC}"
 fi
@@ -346,7 +359,8 @@ export LIBS_DIR=${BUILD_ROOT}/src/libs;
 export APPS_DIR=${BUILD_ROOT}/src/apps;
 export TEST_DIR=${BUILD_ROOT}/test;
 
-chmod u+x -R ${SCRIPTS_DIR}/
+chmod u+x -R "${SCRIPTS_DIR}/"
 
-#by default all libraries and applications are included in the build. This is better tunned when a target build is selected!
+# by default all libraries and applications are included in the build.
+# This is better tunned when a target build is selected!
 __include_all;
